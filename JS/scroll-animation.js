002bb10368aa8e96bc8a7f6a9fe2086f64da2355
Hameed Jimoh <hameed.jimoh1601@gmail.com>
2019-11-28 03:41:00 -0800

    My first commit

---
+++ JS/scroll-animation.js
@@ -0,0 +1,54 @@
+//Top Section header scroll animation
+window.sr = new ScrollReveal();
+        sr.reveal('#you-can-get', {
+            duration: 2000,
+            distance: '200px',
+            origin: 'top'
+        })
+// Top section Paragraph scroll animation
+        sr.reveal('.show-from-left', {
+            duration: 2000,
+            distance: '250px',
+            origin: 'left'
+        })
+//Top section buttons scroll animation
+        sr.reveal('.btn', {
+            duration: 7000,
+            origin: 'bottom'
+        })
+
+        sr.reveal('.slideInLeft', {
+            duration: 2000,
+            origin: 'left',
+            distance: '250px'
+        })
+
+        sr.reveal('.slideInRight', {
+            duration: 2000,
+            origin: 'right',
+            distance: '250px'
+        })
+
+        sr.reveal('.resume-templates', {
+            duration: 3000,
+            origin: 'top',
+            distance: '100px'
+        })
+
+        sr.reveal('.benefit-slideIn-left', {
+            duration: 4000,
+            origin: 'left',
+            distance: '350px'
+        })
+
+        sr.reveal('.cv-img', {
+            duration: 4000,
+            origin: 'right',
+            distance: '250px'
+        })
+
+        sr.reveal('.create-resume-btn', {
+            duration: 5000,
+            origin: 'top',
+            distance: '50px'
+        })
